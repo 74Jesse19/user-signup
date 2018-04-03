594c5c1bdf8073c94ec56bb55a1f21f085e8dcc2
Jesse Williams <jesse_1974@att.net>
2018-04-02 20:01:13 -0500

    fixed data validation

--- main.py
+++ main.py
@@ -6,52 +6,52 @@ app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
 
-@app.route('/welcome', methods=['POST'])
+@app.route('/', methods=['POST'])
 def welcome():
     name_var = request.form['name']
     pw_var = request.form['password']
     pw_var2 = request.form['password2']
-    encoded_error = request.args.get("error")
-    encoded_error3 = request.args.get("error3") 
-    encoded_error2 = request.args.get("error2") 
+
+
+    error = ""
+    error2 = ""
+    error3 = ""
 
  # username data validation  
     if len(name_var)<3 or len(name_var)>20 or (not name_var) or (name_var.strip() == '') or (not(name_var.isdigit()) and not(name_var.isalpha())):
         error = "That is not a valid username"
-        if len(pw_var)<3 or len(pw_var)>20 or (not pw_var) or (pw_var.strip() == '') or (not(pw_var.isdigit()) and not(pw_var.isalpha())):
-            error2 = "That's not a valid password"
-        if len(pw_var2)<3 or len(pw_var2)>20 or (not pw_var2) or (pw_var2.strip() == '') or (not(pw_var2.isdigit()) and not(pw_var2.isalpha())):
-                error3 = "That's not a valid password"    
-        return redirect("/?error=" + error)
-
-        
-    
-    if not(len(name_var)<3 or len(name_var)>20 or (not name_var) or (name_var.strip() == '') or (not(name_var.isdigit()) and not(name_var.isalpha()))):
-        
        
+       
+
         
 # password data validation         
-        if len(pw_var)<3 or len(pw_var)>20 or (not pw_var) or (pw_var.strip() == '') or (not(pw_var.isdigit()) and not(pw_var.isalpha())):
-            error2 = "That's not a valid password"
-            return redirect("/?error=" + error2)
-        if not(len(pw_var)<3 or len(pw_var)>20 or (not pw_var) or (pw_var.strip() == '') or (not(pw_var.isdigit()) and not(pw_var.isalpha()))):
-            
+    if len(pw_var)<3 or len(pw_var)>20 or (not pw_var) or (pw_var.strip() == '') or (not(pw_var.isdigit()) and not(pw_var.isalpha())):
+        error2 = "That's not a valid password"
+       
+       
 
 # verify password data validation            
-            if len(pw_var2)<3 or len(pw_var2)>20 or (not pw_var2) or (pw_var2.strip() == '') or (not(pw_var2.isdigit()) and not(pw_var2.isalpha())):
-                error3 = "That's not a valid password"
-                return redirect("/?error=" + error3)
+    if len(pw_var2)<3 or len(pw_var2)>20 or (not pw_var2) or (pw_var2.strip() == '') or (not(pw_var2.isdigit()) and not(pw_var2.isalpha())):
+        error3 = "That's not a valid password"
+       
 
-           #if not(len(pw_var2)<3 or len(pw_var2)>20 or (not pw_var2) or (pw_var2.strip() == '') or (not(pw_var2.isdigit()) and not(pw_var2.isalpha()))
-            else:
+    if not error and not error2 and not error3:
+         return render_template('welcome.html')
+        #,name=name_var,password=pw_var, password2=pw_var2, error=error, error2=error2, error3=error3)  
+
+
+          
+    else:
+        return render_template('edit.html',name=name_var,password=pw_var,  error=error, error2=error2, error3=error3)
             
-                return render_template('welcome.html',name=name_var,password=pw_var, password2=pw_var2, error=encoded_error, error2=encoded_error2, error3=encoded_error3)    
+         
     
 
 @app.route('/')
 def index():
-    encoded_error = request.args.get("error") 
-    return render_template('edit.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
+    
+    
+    return render_template('edit.html')
     
 
 
